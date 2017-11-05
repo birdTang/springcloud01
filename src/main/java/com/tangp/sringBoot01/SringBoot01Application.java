21e0581686d2d56e47edf80a6c70b21db00008bb
tangpeng <aptx4869_tp@163.com>
2017-11-05 16:32:23 +0800

    创建springboot项目

---
+++ src/main/java/com/tangp/sringBoot01/SringBoot01Application.java
@@ -0,0 +1,12 @@
+package com.tangp.sringBoot01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringBoot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringBoot01Application.class, args);
+	}
+}
