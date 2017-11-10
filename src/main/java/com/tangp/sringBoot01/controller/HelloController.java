fae50d18c6fc38f905f43113aecf163f74117e32
Administrator <Administrator@windows10.microdone.cn>
2017-11-10 16:25:22 +0800

    环境切换，自定义属性

--- src/main/java/com/tangp/sringBoot01/controller/HelloController.java
+++ src/main/java/com/tangp/sringBoot01/controller/HelloController.java
@@ -1,13 +1,25 @@
 package com.tangp.sringBoot01.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	@Value("${book.name}")
+	private String name;
+	@Value("${book.author}")
+	private String author;
+	@Value("${book.all}")
+	private String allmsg;
 	@RequestMapping("/hello")
 	public String index() {
 		return "Hello world";
+	}
+	
+	@RequestMapping("/book")
+	public String book() {
+		return name+":"+author +"===="+allmsg;
 	}
 }
--- src/test/java/com/tangp/sringBoot01/controller/HelloControllerTest.java
+++ src/test/java/com/tangp/sringBoot01/controller/HelloControllerTest.java
@@ -1,16 +1,13 @@
 package com.tangp.sringBoot01.controller;
 
-import javax.net.ssl.SSLEngineResult.Status;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -19,7 +16,7 @@ import com.tangp.sringBoot01.SringBoot01Application;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.equalTo;
 @RunWith(SpringRunner.class)
-@SpringBootTest()
+@SpringBootTest(classes=SringBoot01Application.class)
 public class HelloControllerTest {
 	
 	private MockMvc mockMvc;
@@ -32,6 +29,13 @@ public class HelloControllerTest {
 	@Test
 	public void testIndex() throws Exception{
 		mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(content().string(equalTo("Hello world")));
+	}
+	
+	@Test
+	public void testBook() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.get("/book").accept(MediaType.APPLICATION_JSON))
 		.andExpect(status().isOk())
 		.andExpect(content().string(equalTo("Hello world")));
 	}
