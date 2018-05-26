ca6d4156d5f5c0fe670021ceadf481d3affc540e
tangpeng <aptx4869_tp@163.com>
2018-05-26 11:50:15 +0800

    提交

---
+++ src/main/java/com/tangp/sringBoot01/entity/User.java
@@ -0,0 +1,56 @@
+package com.tangp.sringBoot01.entity;
+
+
+public class User {
+	private Integer id;
+	private int age;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	private static class ThreadTest implements Runnable {
+		
+		private volatile User user;
+		
+		
+		public ThreadTest(User user) {
+			super();
+			this.user = user;
+			
+		}
+
+		public User getUser() {
+			return user;
+		}
+
+		public void setUser(User user) {
+			this.user = user;
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			System.out.println("当前用户age为："+user.getAge());
+		}
+		
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		User user = new User();
+		user.setId(1);
+		for(int i=0;i<3;i++) {
+			user.setAge(i);
+			System.out.println(Thread.currentThread().getId());
+			new Thread(new ThreadTest(user)).start();
+		}
+	}
+}
