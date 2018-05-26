a61c74d814ad1c62c14c8b500c5647171a7ad48d
tangpeng <aptx4869_tp@163.com>
2018-05-26 11:49:56 +0800

    提交

--- src/main/java/com/tangp/sringBoot01/controller/HelloController.java
+++ src/main/java/com/tangp/sringBoot01/controller/HelloController.java
@@ -1,6 +1,11 @@
 package com.tangp.sringBoot01.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +18,21 @@ public class HelloController {
 	private String author;
 	@Value("${book.all}")
 	private String allmsg;
+	@Autowired
+	private DiscoveryClient discoverClient;
+
 	@RequestMapping("/hello")
 	public String index() {
+		List<ServiceInstance> list = discoverClient.getInstances("hello-server");
+		for (ServiceInstance server : list) {
+			System.out.println(
+					"host:" + server.getHost() + "port:" + server.getPort() + "serverid:" + server.getServiceId());
+		}
 		return "Hello world";
 	}
-	
+
 	@RequestMapping("/book")
 	public String book() {
-		return name+":"+author +"===="+allmsg;
+		return name + ":" + author + "====" + allmsg;
 	}
 }
